2c64d0e3ac65bdd22a3a56706ea613444bdd8495
eamiear <justcall_me_k@163.com>
2020-06-24 15:53:28 +0800

    菜单定义

--- src/mock/menu.js
+++ src/mock/menu.js
@@ -61,5 +61,227 @@ export default [
     },
     "name": "enterprise",
     "id": "o7d6e2e4e2934f2c9385a623fd98c6f3"
+  },
+  {
+    "redirect": null,
+    "path": "/package",
+    "component": "Layouts/RouteView",
+    "route": "1",
+    "children": [
+      {
+        "path": "/package/list",
+        "component": "package/PackageList",
+        "route": "1",
+        "meta": {
+          "keepAlive": false,
+          "title": "套餐列表"
+        },
+        "name": "package-list",
+        "id": "4r915b2769fc80648e92d04e84ca059d"
+      }
+    ],
+    "meta": {
+      "keepAlive": false,
+      "icon": "setting",
+      "title": "套餐管理"
+    },
+    "name": "package",
+    "id": "o7d6e2e4e2934f2c9385a623fd98c6f3"
+  },
+  {
+    "redirect": null,
+    "path": "/service",
+    "component": "Layouts/RouteView",
+    "route": "1",
+    "children": [
+      {
+        "path": "/service/list",
+        "component": "service/ServiceContent",
+        "route": "1",
+        "meta": {
+          "keepAlive": false,
+          "title": "服务内容"
+        },
+        "name": "service-content",
+        "id": "4r915b2769fc80648e92d04e84ca059d"
+      },
+      {
+        "path": "/service/record",
+        "component": "service/ServiceRecord",
+        "route": "1",
+        "meta": {
+          "keepAlive": false,
+          "title": "服务记录"
+        },
+        "name": "service-record",
+        "id": "4r915b2769fc80648e92d04e84ca059d"
+      },
+      {
+        "path": "/service/approval",
+        "component": "service/ServiceApproval",
+        "route": "1",
+        "meta": {
+          "keepAlive": false,
+          "title": "服务审批"
+        },
+        "name": "service-approval",
+        "id": "4r915b2769fc80648e92d04e84ca059d"
+      }
+    ],
+    "meta": {
+      "keepAlive": false,
+      "icon": "setting",
+      "title": "服务管理"
+    },
+    "name": "service",
+    "id": "o7d6e2e4e2934f2c9385a623fd98c6f3"
+  },
+  {
+    "redirect": null,
+    "path": "/finance",
+    "component": "Layouts/RouteView",
+    "route": "1",
+    "children": [
+      {
+        "path": "/finance/revenue",
+        "component": "finance/FinanceRevenue",
+        "route": "1",
+        "meta": {
+          "keepAlive": false,
+          "title": "营收总额"
+        },
+        "name": "finance-revenue",
+        "id": "4r915b2769fc80648e92d04e84ca059d"
+      },
+      {
+        "path": "/finance/record",
+        "component": "finance/FinanceRecord",
+        "route": "1",
+        "meta": {
+          "keepAlive": false,
+          "title": "营收记录"
+        },
+        "name": "finance-record",
+        "id": "4r915b2769fc80648e92d04e84ca059d"
+      },
+      {
+        "path": "/finance/arrears",
+        "component": "finance/FinanceArrears",
+        "route": "1",
+        "meta": {
+          "keepAlive": false,
+          "title": "欠费记录"
+        },
+        "name": "finance-arrears",
+        "id": "4r915b2769fc80648e92d04e84ca059d"
+      }
+    ],
+    "meta": {
+      "keepAlive": false,
+      "icon": "setting",
+      "title": "财务管理"
+    },
+    "name": "finance",
+    "id": "o7d6e2e4e2934f2c9385a623fd98c6f3"
+  },
+  {
+    "redirect": null,
+    "path": "/report",
+    "component": "Layouts/RouteView",
+    "route": "1",
+    "children": [
+      {
+        "path": "/report/revenue",
+        "component": "report/ReportRevenue",
+        "route": "1",
+        "meta": {
+          "keepAlive": false,
+          "title": "营收报表"
+        },
+        "name": "report-revenue",
+        "id": "4r915b2769fc80648e92d04e84ca059d"
+      },
+      {
+        "path": "/report/fiance",
+        "component": "report/ReportFinance",
+        "route": "1",
+        "meta": {
+          "keepAlive": false,
+          "title": "财务明细"
+        },
+        "name": "report-fiance",
+        "id": "4r915b2769fc80648e92d04e84ca059d"
+      },
+      {
+        "path": "/report/fiancesum",
+        "component": "report/ReportFinanceSummary",
+        "route": "1",
+        "meta": {
+          "keepAlive": false,
+          "title": "财务汇总"
+        },
+        "name": "report-fiancesum",
+        "id": "4r915b2769fc80648e92d04e84ca059d"
+      }
+    ],
+    "meta": {
+      "keepAlive": false,
+      "icon": "setting",
+      "title": "报表管理"
+    },
+    "name": "report",
+    "id": "o7d6e2e4e2934f2c9385a623fd98c6f3"
+  },
+  {
+    "redirect": null,
+    "path": "/building",
+    "component": "Layouts/RouteView",
+    "route": "1",
+    "children": [
+      {
+        "path": "/building/list",
+        "component": "building/BuildingList",
+        "route": "1",
+        "meta": {
+          "keepAlive": false,
+          "title": "楼宇信息"
+        },
+        "name": "building-list",
+        "id": "4r915b2769fc80648e92d04e84ca059d"
+      }
+    ],
+    "meta": {
+      "keepAlive": false,
+      "icon": "setting",
+      "title": "楼宇管理"
+    },
+    "name": "building",
+    "id": "o7d6e2e4e2934f2c9385a623fd98c6f3"
+  },
+  {
+    "redirect": null,
+    "path": "/message",
+    "component": "Layouts/RouteView",
+    "route": "1",
+    "children": [
+      {
+        "path": "/message/advice",
+        "component": "message/MessageAdvice",
+        "route": "1",
+        "meta": {
+          "keepAlive": false,
+          "title": "反馈建议"
+        },
+        "name": "message-advice",
+        "id": "4r915b2769fc80648e92d04e84ca059d"
+      }
+    ],
+    "meta": {
+      "keepAlive": false,
+      "icon": "setting",
+      "title": "消息管理"
+    },
+    "name": "message",
+    "id": "o7d6e2e4e2934f2c9385a623fd98c6f3"
   }
 ]
